51c0aa0890eb2be7f6011dcb875428aa0ee93a15
Edyta Godniak (ego01) <edyta.godniak@hrs.com>
2020-08-14 13:27:34 +0200

    Add Home page to shorten up App class, add NotFound page

--- github_finder/src/App.js
+++ github_finder/src/App.js
@@ -1,11 +1,11 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
-import Users from "./components/users/Users";
 import User from "./components/users/User";
-import Search from "./components/users/Search";
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
 
 import GihubState from "./context/github/GithubState";
 import AlertState from "./context/alert/AlertState";
@@ -22,23 +22,15 @@ const App = () => {
             <div className='container'>
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={(
-                    props // we have to add Fragment below, if not we would have two parent elements, i.e. Search and Users
-                  ) => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route
                   exact
                   path='/user/:login' //route param login, we will have access to id in our route (i.e. User) in props.match.params.login )
                   component={User}
+                />
+                <Route
+                  component={NotFound} // default value, if anything else in the path go to NotFound
                 />
               </Switch>
             </div>
